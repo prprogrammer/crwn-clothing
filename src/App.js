14a14844cf0e7fbee24f04797220448fe983bce2
prprogrammer <pageprabhat@gmail.com>
2019-11-05 12:39:11 +0530

    new items folder

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
-
-const HatsPage = props => (
-  <div>
-    <h1>HATS PAGE</h1>
-    {console.log(props)}
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
